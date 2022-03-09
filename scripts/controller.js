cfb7d14766a80fbfdba4fd28af6d154f425c4c19
mateuszNadolny <mateusznadolny00@gmail.com>
2022-03-09 21:43:24 +0100

    styling fixed, map is rendering, app is ready for developing rest of the logic

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,8 +1,10 @@
 import SidebarView from "./views/sidebarView.js";
+import MapView from "./views/mapView.js";
 
 
 const init = function () {
     SidebarView.handleSidebarVisibility();
+    MapView.renderMap();
 }
 
 
--- scripts/views/mapView.js
+++ scripts/views/mapView.js
@@ -1,15 +1,19 @@
 class MapView {
 
     renderMap() {
-        var map = L.map('map').setView([51.505, -0.09], 13);
+        const map = L.map('map').setView([51.505, -0.09], 13);
 
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+            maxZoom: 20,
+            attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
         }).addTo(map);
 
         L.marker([51.5, -0.09]).addTo(map)
             .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
             .openPopup();
-    }
 
+        console.log('mapa')
+    }
 }
+
+export default new MapView();

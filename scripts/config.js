e817f20016bca655749b10d502a48dc2d888b965
Mateusz Nadolny <mateusznadolny00@gmail.com>
2022-03-22 14:21:08 +0100

    updating API url

--- scripts/config.js
+++ scripts/config.js
@@ -1,3 +1,3 @@
-export const API_URL = 'http://ip-api.com/json';
+export const API_URL = 'https://geo.ipify.org/api/v2/country,city?apiKey=at_tzEm6zMBxJ6F3f7kPl5Bt9PGrjScW&ipAddress';
 export const TIMEOUT_SEC = 10000;
 

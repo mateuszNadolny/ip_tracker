c753ab0c154d23836a7ccd378d7a8decf19ea6c7
Mateusz Nadolny <mateusznadolny00@gmail.com>
2022-03-22 15:40:52 +0100

    Update mapView.js

--- scripts/views/mapView.js
+++ scripts/views/mapView.js
@@ -13,9 +13,8 @@ class MapView {
     
     renderMap() {
         this._map = L.map('map').setView(this._userCoords, 13);
-        L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
-            maxZoom: 20,
-            attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'        
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(this._map);
 
         this._renderUserMarker(this._userCoords);
@@ -71,8 +70,7 @@ class MapView {
 
 export default new MapView();
 
-//  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+
 
 
 
